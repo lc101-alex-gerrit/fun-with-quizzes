d0e93a55ed404e996e77cc2c6b6ca0cd92ae4f0b
Lb-LC101 <lisabartfeld@gmail.com>
2021-01-28 20:38:13 -0600

    completed TrueOrFalse

--- src/org/launchcode/alexandgerrit/funwithquizzes/TrueOrFalse.java
+++ src/org/launchcode/alexandgerrit/funwithquizzes/TrueOrFalse.java
@@ -1,4 +1,17 @@
 package org.launchcode.alexandgerrit.funwithquizzes;
 
 public class TrueOrFalse extends Question {
+
+    public TrueOrFalse (String question, String correctAnswer) {
+        super(question, correctAnswer);
+    }
+
+    @Override
+    public boolean checkAnswer() {
+        String userAnswer = getAnswerFromUser();
+        if (userAnswer.equals(getCorrectAnswer())) {
+            return true;
+        }
+        return false;
+    }
 }
